59f23dd25a2de273a5742cb6b7aafe5742007f72
Julia <juliamishchenko12@gmail.com>
2016-05-07 20:07:52 +0300

    Initial commit, added main loader, by Julia

---
+++ src/main/java/modeling/Loader.java
@@ -0,0 +1,12 @@
+package modeling;
+
+/**
+ * Created by Julia on 07.05.2016.
+ */
+public class Loader {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+    }
+}
