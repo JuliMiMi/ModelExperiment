e4f1991e92e4eca3588711ba3bf6ac85412a7683
Julia <juliamishchenko12@gmail.com>
2016-05-15 19:03:06 +0300

    Add rows in a matrix, by Julia"

--- src/main/java/modeling/ui/view/indicator/IndicatorMatrixView.java
+++ src/main/java/modeling/ui/view/indicator/IndicatorMatrixView.java
@@ -28,24 +28,38 @@ public class IndicatorMatrixView extends JPanel implements View {
     public JTable buildCompetitivenessMatrix(Collection<CompanyModel> companyModels) {
         JPanel competitivenessMatrix = new JPanel(new BorderLayout());
 
-        Object[] header = {" ", "Indicators", "CR", "CW", "CM", "CP", "CT"};
+        Object[] header = {" ", "Indicators", "CR", " ", "CW", " ", "CM", " ", "CP", " ", "CT", " " };
         Object[][] standardData = {
-                {"№", " ", "1", "2", "3", "4", "5"},
-                {"levels of competitiveness", "rel.un", "point", "rel.un", "point", "rel.un", "point", "rel.un", "point", "rel.un", "point"},
-                {"1", "Global leadership", "5", "100", "3.5", "100", "0.5", "100", "1", "100", "0.7", "100"},
-                {"2", "World standard", "3.81", "76.2", "2.8", "80", "0.44", "88", "0.9", "90", "0.62", "88.57"},
-                {"3", "National leadership", "2.94", "58.8", "2.5", "71.43", "0.36", "72", "0.78", "78", "0.46", "65.71"},
-                {"4", "National standard", "2.46", "49.2", "2.4", "68.57", "0.3", "60", "0.66", "66", "0.36", "51.43"},
-                {"5", "Sectoral leadership", "2.18", "43.6", "2.06", "58.86", "0.19", "38", "0.51", "51", "0.27", "38.57"},
-                {"6", "Industry standards", "1.67", "33.4", "1.6", "45.71", "0.07", "14", "0.38", "38", "0.15", "21.43"},
-                {"7", "Threshold level", "1.33", "26.6", "1", "28.57", "0.01", "2", "0.15", "15", "0.09", "12.86"},
+                {"№", " ", "1", " ", "2", " ", "3", "4", " ", "5", " ",},
+                {" ", "Levels of competitiveness", "rel.un", "point", "rel.un", "point", "rel.un", "point", "rel.un", "point", "rel.un", "point" },
+                {"1", "Global leadership", "5", "100", "3.5", "100", "0.5", "100", "1", "100", "0.7", "100" },
+                {"2", "World standard", "3.81", "76.2", "2.8", "80", "0.44", "88", "0.9", "90", "0.62", "88.57" },
+                {"3", "National leadership", "2.94", "58.8", "2.5", "71.43", "0.36", "72", "0.78", "78", "0.46", "65.71" },
+                {"4", "National standard", "2.46", "49.2", "2.4", "68.57", "0.3", "60", "0.66", "66", "0.36", "51.43" },
+                {"5", "Sectoral leadership", "2.18", "43.6", "2.06", "58.86", "0.19", "38", "0.51", "51", "0.27", "38.57" },
+                {"6", "Industry standards", "1.67", "33.4", "1.6", "45.71", "0.07", "14", "0.38", "38", "0.15", "21.43" },
+                {"7", "Threshold level", "1.33", "26.6", "1", "28.57", "0.01", "2", "0.15", "15", "0.09", "12.86" },
         };
 
         DefaultTableModel defaultTableModel = new DefaultTableModel(standardData, header);
 
+
         for (CompanyModel companyModel : companyModels) {
             defaultTableModel.addRow(new Object[]{
-                    //TODO ADD DYNAMIC STUFF FOR COMPANIES
+
+                    defaultTableModel.getRowCount() - 1,
+                    companyModel.getName(),
+                    companyModel.getIndRes(),
+                    companyModel.getIndRes() * 26.6 / 1.33,
+                    companyModel.getIndWork(),
+                    companyModel.getIndWork() * 28.57 / 1,
+                    companyModel.getIndMar(),
+                    companyModel.getIndMar() * 2 / 0.01,
+                    companyModel.getIndRent(),
+                    companyModel.getIndRent() * 15 / 0.15,
+                    companyModel.getIndTime(),
+                    companyModel.getIndTime() * 12.86 / 0.09
+
             });
         }
 
@@ -70,7 +84,7 @@ public class IndicatorMatrixView extends JPanel implements View {
     }
 
     private JTable buildSummaryMatrix() {
-        //TODO summary matrix
+        Object[] header = {"Levels of competitiveness" };
         return null;
     }
 
